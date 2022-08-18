34b9bc1c12d809029342454b5a9d922edd50a3c4
pro.boooooo <pro.boudjemline@gmail.com>
2022-08-18 02:43:51 +0200

    $

--- client/config.lua
+++ client/config.lua
@@ -1,13 +1,52 @@
 Config = {}
 
--- Position to open veh
-Config.range = 5
+-- Marker
+Config.Marker = 23
+Config.keymastercoords = vector3(170.6294708252, -1799.0465087891, (29.235868453979-0.99))
+Config.color = {r = 255, g = 0, b = 0}
+Config.rangeToInteract = 4
 
--- Vehicule statut
-Config.canOpen = boolean
+-- price
+Config.price = 2000
 
 -- Key
-Config.useKey = 73
+Config.Key = 51
 
--- Ab tester plus tard
-Config.press = 55
+-- Truc en haut de l'ecran lol
+Config.beforeInteract = "~b~Appuyez sur ~INPUT_CONTEXT~ pour parler au serurrier"
+
+-- RageUI
+Config.title = "Serrurier"
+Config.blanksubandtitle = " "
+
+Config.desc1 = "Pour fabriquer un doublon"
+Config.desc2 = "Pour jeter un doublon"
+Config.desc3 = "Pour changer la serrure du véhicule"
+
+Config.KeyHomeMenu = "Créer un doublon"
+Config.KeyHomeMenuSub = "Pour :"
+Config.confirmmoney = "Payer en liquide ~g~("..Config.price.." $)"
+Config.confirmbank = "Payer par carte de crédit ~b~("..Config.price.." $)"
+
+Config.DestroyKey = "Détruire un doublon"
+Config.DestroyKeySub = "Détruire le double de :"
+Config.sure = "Êtes-vous sur de vouloir supprimer le double : ["
+
+Config.KeyChange = "Changer la serrure de votre véhicule"
+Config.KeyChangeSub = "Changer la serrure de :"
+Config.sureChange = "Êtes-vous sur de vouloir supprimer les doubles : ["
+
+Config.oui = "Oui"
+Config.non = "Non"
+Config.cancel = "Annuler"
+Config.noVehCopy = "~r~Vous n'avez pas de doublons !"
+
+-- Blips
+Config.blips = {
+    title = "Serrurier", 
+    color = 0, 
+    id = 134,
+    x = 170.6294708252, 
+    y = -1799.0465087891, 
+    z = 29.235868453979
+}
--- server/s_main.lua
+++ server/s_main.lua
@@ -1,6 +1,120 @@
-RegisterNetEvent("Zod#8682::checkVehOwnerSend")
+RegisterNetEvent("Zod#8682::vehListSend")
+RegisterNetEvent("Zod#8682::makeCashSend")
+RegisterNetEvent("Zod#8682::destroyVeh")
+RegisterNetEvent("Zod#8682::vehCopyListSend")
+RegisterNetEvent("Zod#8682::destroyAllVeh")
 
-AddEventHandler("Zod#8682::checkVehOwnerSend", function(veh)
+Config = {}
+Config.err = {}
+
+Config.price = 2000
+
+Config.ok = "~g~Le doublon pour : "
+Config.ok2 = "~g~ a bien été crée.\n~r~(-"..Config.price.." $)"
+
+Config.ok3 = "~r~Votre doublon ~b~["
+Config.ok4 = "]~r~ a bien été supprimé !"
+
+Config.ok5 = "~r~Les doublons pour ~b~["
+Config.ok6 = "]~r~ ont bien été supprimés !"
+
+Config.noVeh = "~r~Vous n'avez pas de vehicule !"
+Config.noCopy = "~r~Vous n'avez pas de copie de vehicule !"
+
+Config.err.gen = "~r~ERROR"
+Config.err.mcs = "~r~ERROR :: Zod#8682::makeCashSend"
+Config.err.vls = "~r~ERREUR :: Zod#8682::vehListSend"
+Config.err.vcls = "~r~ERREUR :: Zod#8682::vehCopyListSend"
+
+ESX = nil
+TriggerEvent("esx:getSharedObject", function(obj) ESX = obj end)
+
+-- Sache que tu es un gros ksos enefsjbfkesf
+
+AddEventHandler("Zod#8682::vehListSend", function() 
     local _src = source
-    local xPlayer = ESX.GetPlayerFromId(_scr)
+    local xPlayer = ESX.GetPlayerFromId(_src)
+    local steam = xPlayer.identifier
+
+    if(xPlayer ~= nil) then
+        MySQL.Async.fetchAll("SELECT plate FROM owned_vehicles WHERE owner=@o ORDER BY plate ASC", {["o"] = steam}, function(data) 
+            if(#data > 0) then
+                TriggerClientEvent("Zod#8682::vehListReceive", _src, data)
+            else
+                TriggerClientEvent("Zod#8682::vehListReceive", _src, {Config.noVeh})
+            end
+        end)
+    else
+        TriggerClientEvent('esx:showNotification', _src, Config.err.vls)
+    end
+end)
+
+AddEventHandler("Zod#8682::vehCopyListSend", function() 
+    local _src = source
+    local xPlayer = ESX.GetPlayerFromId(_src)
+    local steam = xPlayer.identifier
+
+    if(xPlayer ~= nil) then
+        MySQL.Async.fetchAll("SELECT plaque, record FROM drp_keycopy WHERE identifier=@o ORDER BY plaque ASC", {["o"] = steam}, function(data) 
+            if(#data > 0) then
+                TriggerClientEvent("Zod#8682::vehCopyListReceive", _src, data)
+            else
+                TriggerClientEvent("Zod#8682::vehCopyListReceive", _src, {Config.noCopy})
+            end
+        end)
+    else
+        TriggerClientEvent('esx:showNotification', _src, Config.err.vcls)
+    end
+end)
+
+AddEventHandler("Zod#8682::makeCashSend", function(typeaccount, plate)
+    local _src = source
+    local xPlayer = ESX.GetPlayerFromId(_src)
+    local steam = xPlayer.identifier
+
+    if(typeaccount ~= "money" and typeaccount ~= "bank") then
+        TriggerClientEvent('esx:showNotification', _src, Config.err.gen)
+        return
+    end
+    
+    if(xPlayer ~= nul) then
+        if(typeaccount == "money") then
+            xPlayer.removeMoney(Config.price)
+            MySQL.Async.execute("INSERT INTO drp_keycopy(identifier, plaque) VALUES(@o, @p)", {["o"] = steam, ["p"] = plate}, function() 
+                TriggerClientEvent('esx:showNotification', _src, Config.ok.."~b~["..plate.."]"..Config.ok2.." ~r~: Liquide")
+                return
+            end)
+            return
+        end
+
+        if(typeaccount == "bank") then
+            xPlayer.removeAccountMoney("bank", Config.price)
+            MySQL.Async.execute("INSERT INTO drp_keycopy(identifier, plaque) VALUES(@o, @p)", {["o"] = steam, ["p"] = plate}, function() 
+                TriggerClientEvent('esx:showNotification', _src, Config.ok.."~b~["..plate.."]"..Config.ok2.." ~r~: Banque")
+                return
+            end)
+            return
+        end
+    else
+        TriggerClientEvent('esx:showNotification', _src, Config.err.mcs)
+        return
+    end
+end)
+
+AddEventHandler("Zod#8682::destroyVeh", function(record, plate)
+    local _src = source
+
+    MySQL.Async.execute("DELETE FROM drp_keycopy WHERE record=@r", {["r"] = record}, function()
+        TriggerClientEvent('esx:showNotification', _src, Config.ok3..plate..Config.ok4)
+        return
+    end)
+end)
+
+AddEventHandler("Zod#8682::destroyAllVeh", function(plate) 
+    local _src = source
+
+    MySQL.Async.execute("DELETE FROM drp_keycopy WHERE plaque=@p", {["p"] = plate}, function()
+        TriggerClientEvent('esx:showNotification', _src, Config.ok5..plate..Config.ok6)
+        return
+    end)
 end)
